495201c39ac95300e7383aaa68e2927fe9b90102
marianavalladolid8 <lx05420@innovaccion.mx>
2023-01-03 19:34:07 -0600

    Primer comentario

---
+++ Index.js
@@ -0,0 +1,29 @@
+var textoDigitado = document.querySelector("#area-texto"); 
+var textoGravado = document.querySelector("#texto-saida");
+
+function encriptar(){
+    var texto = textoDigitado.value; 
+    
+    var resultadoCrip = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai")
+    .replace(/o/g, "ober").replace(/u/g, "ufat");
+    
+    document.getElementById("texto-saida").innerHTML ='<textarea  id="resultado" >'+ resultadoCrip +'</textarea>' +
+     '<button id="bt-copiar" onclick="copiar()">Copiar</button>';
+   
+};
+function desencrptar(){
+    var texto = textoDigitado.value;
+
+    var resultadoDesc = texto.replace(/enter/g, "e").replace(/imes/g, "e").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    document.getElementById("texto-saida").innerHTML = '<textarea  id="resultado">'+ resultadoDesc +'</textarea>' +
+     '<button id="bt-copiar" onclick="copiar()">Copiar</button>';
+};
+
+function copiar() {
+    var textoCopiado = document.getElementById('resultado');
+  
+    textoCopiado.select();
+    document.execCommand('copy');
+    alert("Texto copiado.");
+} 
